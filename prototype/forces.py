1d724285311edc4b6155a1b026d5fb1a6fda010f
JonasMuhlenkamp <jonasmuhlenkamp@gmail.com>
2025-02-25 03:15:14 -0500

    commenting out debug prints

--- prototype/forces.py
+++ prototype/forces.py
@@ -16,7 +16,7 @@ def f_grav(a: Particle, b: Particle) -> float:
     # Avoid unbounded acceleration. 
     if d > TOL:
         mag = G * a.get("mass") * b.get("mass") / d**2
-        print("grav: ", mag)
+        # print("grav: ", mag)
         return mag
     else:
         return 0
@@ -29,11 +29,11 @@ def f_elec(a: Particle, b: Particle) -> float:
     ke = 1#8.99e9     # Coulomb constant                
 
     d = np.linalg.norm(b.get("pos") - a.get("pos")) 
-    print(d)
+    # print(d)
     # Avoid unbounded acceleration. 
     if d > TOL:
         mag = ke * a.get("e_charge") * b.get("e_charge") / d**2
-        print(mag)
+        # print(mag)
         return mag
     else:
         return 0
